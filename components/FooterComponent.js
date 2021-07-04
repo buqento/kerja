3127b47c27d84a8033f542717c89c85a3bde7bdf
buqento <buqento@gmail.com>
2021-07-04 11:11:07 +0700

    add backlink

--- components/FooterComponent.js
+++ components/FooterComponent.js
@@ -4,7 +4,7 @@ function FooterComponent() {
         <div className="pt-3">
             <MapComponent />
             <div className="p-3">
-                <small>Copyright &copy; 2021 All Rights Reserved<br />Dinas Penanaman Modal dan PTSP Kabupaten Maluku Barat Daya</small>
+                <small>Copyright &copy; 2021 All Rights Reserved &middot; Powered by <a href="https://tantekos.com" target="_blank">bvqento</a><br />Dinas Penanaman Modal dan PTSP Kabupaten Maluku Barat Daya</small>
             </div>
         </div>
     )
